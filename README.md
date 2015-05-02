4efbded32fc2a1757d7652ec2c960d84a2fe4c4f
Calvin Cheng <linchuan.cheng@gmail.com>
2015-05-02 23:11:49 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DateTime computations in Objective-C
--- TimeAfterTime/main.m
+++ TimeAfterTime/main.m
@@ -10,8 +10,41 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        NSDate *now = [NSDate date];
+        NSLog(@"This NSDate object lives at %p", now);
+        NSLog(@"The date is %@", now);
+        
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds, it will be %@", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSLog(@"My calendar is %@", [cal calendarIdentifier]);
+        
+        unsigned long day = [cal ordinalityOfUnit:NSCalendarUnitDay
+                                           inUnit:NSCalendarUnitMonth
+                                          forDate:now];
+        NSLog(@"This is day %lu of the month", day);
+        
+        unsigned units = NSCalendarUnitWeekday | NSCalendarUnitMonth | NSCalendarUnitDay;
+        NSDateComponents *comps = [cal components:units
+                                         fromDate:now];
+
+        NSInteger monthDay = [comps day];
+        NSLog(@"Day of the Month: %ld", (long)monthDay);
+        
+        NSInteger month = [comps month];
+        NSLog(@"Month: %ld", (long)month);
+        
+        NSInteger weekday = [comps weekday];
+        NSLog(@"Day of the week: %ld", (long)weekday-1);
+        
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        NSString *dayString = [[formatter weekdaySymbols] objectAtIndex:(weekday-1)];
+        NSLog(@"Day of the week in string: %@", dayString);
     }
     return 0;
 }
